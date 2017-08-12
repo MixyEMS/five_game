64dfefa722c8a0596fdc818e13071f8970363aae
Mixy <835410991@qq.com>
2017-08-12 10:20:13 +0800

    1

--- js/script.js
+++ js/script.js
@@ -4,17 +4,19 @@ var ctx = chess.getContext("2d");
 var w = chess.width;
 var h = chess.height;
 
-var l = 15;
-var bw = 30;
+var l = 15;  //棋盘维度
+var bw = 30;  //棋盘边框
 
-var bl = (w-2*bw)/(l-1);
-var r = bl/2-5;
+var bl = (w-2*bw)/(l-1); // 棋盘间距
+var r = bl/2-5;      //棋子半径
 
 var black = {col1:"#0A0A0A",col2:"#636766"};
 var white = {col1:"#CCC",col2:"#fff"}
 
 var me = true;
 var rate = chess.width/chess.offsetWidth;
+
+var over = false;
 
 var chessList = [];
 
@@ -23,6 +25,68 @@ for(var i = 0; i<l;i++){
 	for(var j =0;j<l;j++){
 		chessList[i][j]=0;
 	}
+}
+
+
+//赢法数组
+//
+var wins = [];
+
+for(var i = 0 ;i<l ;i++){
+  wins[i]=[];
+  for(var j =0;j<l;j++){
+     wins[i][j]=[];
+  }
+}
+
+var count = 0;
+//横线赢法
+for(var i =0;i<l;i++){
+   for(var j =0;j<l-4;j++){
+     for(var k =0;k<5;k++){
+        wins[i][j+k][count] = true;
+     }
+     count++;
+   }
+}
+//竖线赢法
+for(var i =0;i<l-4;i++){
+   for(var j =0;j<l;j++){
+     for(var k =0;k<5;k++){
+        wins[i+k][j][count] = true;
+     }
+     count++;
+   }
+}
+
+//反斜线赢法
+for(var i =0;i<l-4;i++){
+   for(var j =0;j<l-4;j++){
+     for(var k =0;k<5;k++){
+        wins[i+k][j+k][count] = true;
+     }
+     count++;
+   }
+}
+
+for(var i =0;i<l-4;i++){
+   for(var j =4;j<l;j++){
+     for(var k =0;k<5;k++){
+        wins[i+k][j-k][count] = true;
+     }
+     count++;
+   }
+}
+
+
+//console.log(count); 572
+//赢法统计数组
+//
+var myWin = [];
+var computerWin = [];
+for(var i = 0 ;i<count ;i++){
+   myWin[i] = 0;
+   computerWin[i]=0;
 }
 
 function drawChessBoard(){
@@ -53,13 +117,10 @@ function drawChess(i,j,me){
    ctx.fill();
 }
 
-function oneStep(i,j){
+function oneStep(i,j,chess){
 
-     if(!chessList[i][j]){
-     	drawChess(i,j,me?black:white);
-        me=!me;
-        chessList[i][j]=1;
-     }
+       	drawChess(i,j,chess);
+       
 }
 
 function chessInit(){
@@ -67,11 +128,38 @@ function chessInit(){
 }
 
 chess.onclick = function(e){
+     if(over){
+      return;
+     }
      var x = e.offsetX;
      var y = e.offsetY;
 
      var i = Math.floor(rate*x/bl);
      var j = Math.floor(rate*y/bl);
     
-     oneStep(i,j);
+     if(!chessList[i][j]){
+         if(me){
+            oneStep(i,j,black);
+            chessList[i][j]=1;
+
+            for(var k = 0 ; k<count;k++){
+               if(wins[i][j][k]){
+                    myWin[k]++;
+                    computerWin[k]=6;
+
+                    if(myWin[k]==5){
+                      alert("你赢啦！");
+                       over = true;
+                    }
+                 }
+             }
+         }else{
+            oneStep(i,j,white);
+            chessList[i][j]=2;
+         }
+
+         me = !me;
+     }
+     
+    
 }
